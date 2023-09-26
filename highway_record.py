68ba8881a265ce17abd16187db4ab27033d59805
wulonmt <zuck0087285@gmail.com>
2023-09-26 21:45:29 +0800

    add 4 frame image

--- highway_record.py
+++ highway_record.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--log_model", help="modle to be logged", type=str)
 args = parser.parse_args()
 ENV_LIST=["merge", "highway", "racetrack", "roundabout", "intersection",]
-RECORD = True
+RECORD =False
 
 if __name__ == "__main__":
     log_env = args.log_model.split('/')[0].split('_')[0]
@@ -42,3 +42,11 @@ if __name__ == "__main__":
             action, _ = model.predict(obs)
             obs, reward, done, truncated, info = env.step(action)
             env.render()
+    env.reset()
+    for _ in range(3):
+        obs, reward, done, truncated, info = env.step(env.action_type.actions_indexes["IDLE"])
+
+        fig, axes = plt.subplots(ncols=4, figsize=(12, 5))
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(obs[i, ...].T, cmap=plt.get_cmap('gray'))
+    plt.show()
